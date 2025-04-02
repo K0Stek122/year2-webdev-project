abab4c644c36ffcdf350dc192117ba84b57e4931
K0Stek122 <kamilianos3@gmail.com>
2025-04-02 15:09:27 +0100

    Fetch items from the database and display them in the ItemPool component

--- amplitude-web-dev/src/components/ItemPool.tsx
+++ amplitude-web-dev/src/components/ItemPool.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import {useState} from 'react';
+import { readData } from '../FirebaseHandler';
 import './css/ItemPool.css';
 import './SaleItem'
 import SaleItem from './SaleItem';
 
 type ItemPoolProps = object;
 
+interface Item {
+    id: string;
+    name: string;
+    price: string;
+    tag: string;
+}
+
 const ItemPool: React.FC<ItemPoolProps> = () => {
+    const [items, setItems] = useState<Item[]>([]);
+    const fetchDatabaseItems = () => {
+        // Fetch items from the database
+        readData('items').then((data) => {
+            const itemsTemp: Item[] = Object.entries(data).map(([id, itemData]) => ({
+                id,
+                name: itemData.name,
+                price: parseFloat(itemData.price),
+                tag: itemData.tag,
+            }));
+            setItems(itemsTemp);
+        });
+    }
+
+    fetchDatabaseItems();
+
     return (
         <div className="item-pool">
-            {Array.from({ length: 5 }).map((_, index) => (
-                <SaleItem key={index} name={`Item ${index + 1}`} price={Math.floor(Math.random() * 100) + 1} />
+            {items.map((item) => (
+            <SaleItem name={item.name} price={Number(item.price)} />
             ))}
         </div>
     );
